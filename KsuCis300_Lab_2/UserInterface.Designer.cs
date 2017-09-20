883fa88b0911b193169b24003eb3f36cbcf8cf36
Gabriel Serrano <gjserrano@ksu.edu>
2017-09-19 19:23:25 -0500

    Add project files.

---
+++ KsuCis300_Lab_2/UserInterface.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace KsuCis300_Lab_2
+{
+    partial class UserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uxMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxOpenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.uxSaveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.uxOpenFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.uxSaveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.uxMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // uxMenuStrip
+            // 
+            this.uxMenuStrip.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.uxMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.uxMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.uxMenuStrip.Name = "uxMenuStrip";
+            this.uxMenuStrip.Size = new System.Drawing.Size(826, 42);
+            this.uxMenuStrip.TabIndex = 0;
+            this.uxMenuStrip.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.uxOpenToolStripMenuItem,
+            this.uxSaveAsToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(64, 38);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // uxOpenToolStripMenuItem
+            // 
+            this.uxOpenToolStripMenuItem.Name = "uxOpenToolStripMenuItem";
+            this.uxOpenToolStripMenuItem.Size = new System.Drawing.Size(268, 38);
+            this.uxOpenToolStripMenuItem.Text = "Open...";
+            this.uxOpenToolStripMenuItem.Click += new System.EventHandler(this.uxOpenToolStripMenuItem_Click);
+            // 
+            // uxSaveAsToolStripMenuItem
+            // 
+            this.uxSaveAsToolStripMenuItem.Name = "uxSaveAsToolStripMenuItem";
+            this.uxSaveAsToolStripMenuItem.Size = new System.Drawing.Size(268, 38);
+            this.uxSaveAsToolStripMenuItem.Text = "Save As...";
+            this.uxSaveAsToolStripMenuItem.Click += new System.EventHandler(this.uxSaveAsToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 66);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 474);
+            this.textBox1.TabIndex = 1;
+            // 
+            // uxOpenFileDialog
+            // 
+            this.uxOpenFileDialog.FileName = "openFileDialog1";
+            // 
+            // UserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(826, 571);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.uxMenuStrip);
+            this.MainMenuStrip = this.uxMenuStrip;
+            this.Name = "UserInterface";
+            this.Text = "Form1";
+            this.uxMenuStrip.ResumeLayout(false);
+            this.uxMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip uxMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uxOpenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem uxSaveAsToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog uxOpenFileDialog;
+        private System.Windows.Forms.SaveFileDialog uxSaveFileDialog;
+    }
+}
+
---
+++ KsuCis300_Lab_2/UserInterface.cs
@@ -0,0 +1,52 @@
+﻿/*
+ * KsuCis300_Lab_2.cs
+ * Author: Gabriel Serrano
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KsuCis300_Lab_2
+{
+    public partial class UserInterface : Form
+    {
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// The event handler for the "Open..." section of uxMenuStrip
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxOpenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFile = new OpenFileDialog();
+
+            string fileName = openFile.FileName;
+
+            if(openFile.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Error: Cannot Open File!");
+            }
+        }
+
+        private void uxSaveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFile = new SaveFileDialog();
+
+            if(saveFile.ShowDialog() == DialogResult.OK)
+            {
+                MessageBox.Show("Error: Cannot Save to File " + saveFile.FileName);
+            }
+        }
+    }
+}
